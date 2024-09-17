a639f38ed85192c6bf64f5c302c747cb0b6f8423
kylo ren <oneearl706@gmail.com>
2024-09-17 22:50:05 +0800

    Initialized model for tournaments

--- tournaments/models.py
+++ tournaments/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+
+from datetime import timedelta
 
 # Create your models here.
+
+
+class Tournament(models.Model):
+    tournament_name = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+    sport = models.CharField(max_length=255)
+    date_created = models.DateTimeField(default=timezone.now())
+    tournament_start = models.DateTimeField(
+        default=timezone.now, auto_now=False, auto_now_add=False
+    )
+    tournament_end = models.DateTimeField(
+        default=timezone.now() + timedelta(days=5), auto_now=False, auto_now_add=False
+    )
+    bracketing_system = models.CharField(max_length=255)
+    # requirements = models.ArrayField()
